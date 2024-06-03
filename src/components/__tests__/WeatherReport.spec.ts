ae38d41ebf44b8329916700d90f76257476aa61e
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2024-06-03 22:00:08 +0700

    [TEST] create new test case for WeatherReport.spec.ts
    - display weather data when data is defined

--- src/components/__tests__/WeatherReport.spec.ts
+++ src/components/__tests__/WeatherReport.spec.ts
@@ -1,4 +1,4 @@
-import { shallowMount } from '@vue/test-utils'
+import { flushPromises, mount, shallowMount } from '@vue/test-utils'
 import { describe, vi } from 'vitest'
 import WeatherReport from '../WeatherReport.vue'
 
@@ -33,5 +33,57 @@ describe('WeatherReport', () => {
     })
 
     expect(wrapper.text()).toContain('Loading...')
+  })
+
+  it('displays weather data when data is defined', async () => {
+    const mockData = {
+      location: {
+        localtime: new Date(),
+        name: 'Eindhoven',
+        region: 'North Brabant',
+        country: 'Netherlands',
+        lat: 52.37,
+        lon: 4.9
+      },
+      current: {
+        temp_c: 10,
+        temp_f: 50,
+        humidity: 50,
+        precip_in: 0.1,
+        precip_mm: 2.5,
+        condition: {
+          text: 'Sunny',
+          icon: 'https://via.placeholder.com/150'
+        },
+        wind_degree: 180,
+        wind_dir: 'S',
+        wind_kph: 10,
+        wind_mph: 6.2
+      }
+    }
+
+    global.fetch = vi.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockData)
+      })
+    ) as any
+
+    const wrapper = mount(WeatherReport, {
+      props: {
+        coords: {
+          latitude: 0,
+          longitude: 0
+        }
+      }
+    })
+
+    await flushPromises()
+
+    expect(wrapper.text()).toContain(mockData.current.condition.text)
+    expect(wrapper.text()).toContain(mockData.current.temp_c)
+    expect(wrapper.text()).toContain(mockData.location.name)
+    expect(wrapper.text()).toContain(mockData.location.region)
+    expect(wrapper.text()).toContain(mockData.current.wind_kph)
+    expect(wrapper.text()).toContain(mockData.current.wind_degree)
   })
 })
